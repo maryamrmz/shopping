e0a063f631ab5481c5f79dae096cc72594c20869
Maryam Ramezanpoor <maryam.rpour@gmail.com>
2020-03-30 19:49:24 +0430

    update shop data with new data, update cart utils to group itemart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(...middlewares),
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
 export default store;
