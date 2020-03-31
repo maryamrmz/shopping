e2e997f84475aefeba0a2e2ff01e56f82f4aab9a
Maryam Ramezanpoor <maryam.rpour@gmail.com>
2020-03-31 21:25:13 +0430

    add addItem and removeItem functionality to checkout item component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -11,27 +11,29 @@ import { toggleCartHidden } from "../../redux/cart/cart.action";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems, history, dispatch }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
             {cartItems.length ? (
                 cartItems.map(cartItem => (
                     <CartItem key={cartItem.id} item={cartItem} />
                 ))
             ) : (
-                    <span className="empty-message">Your cart is empty</span>
+                    <span className='empty-message'>Your cart is empty</span>
                 )}
         </div>
-        <CustomButton onClick={() => {
-            history.push("/checkout");
-            dispatch(toggleCartHidden())
-        }}
-        >GO TO CHECKOUT
+        <CustomButton
+            onClick={() => {
+                history.push("/checkout");
+                dispatch(toggleCartHidden());
+            }}
+        >
+            GO TO CHECKOUT{" "}
         </CustomButton>
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
-})
+});
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
