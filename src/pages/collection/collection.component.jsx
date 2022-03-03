91bce1385e28085310e7184d3efa199d1fd94673
maryam.rpour@gmail.com <maryam.rpour@gmail.com>
2022-03-04 00:54:57 +0330

    handle undefined mode of object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -12,12 +12,15 @@ import {
 } from './collection.styles';
 
 const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+  const { title, items } = collection || {};
+
   return (
     <CollectionPageContainer>
-      <CollectionTitle>{title}</CollectionTitle>
+      {title && (
+        <CollectionTitle>{title}</CollectionTitle>
+      )}
       <CollectionItemsContainer>
-        {items.map(item => (
+        {items && items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </CollectionItemsContainer>
