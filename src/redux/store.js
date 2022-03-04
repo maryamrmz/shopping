314ce9b05f37672af2d4869ed2a0cf83b255b0be
maryam.rpour@gmail.com <maryam.rpour@gmail.com>
2022-03-04 12:39:17 +0330

    fix: create variable of object before export that

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
-import rootReducer from './root-reducer';
-import rootSaga from './root-saga';
+import rootReducer from "./root-reducer";
+import rootSaga from "./root-saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const middlewares = [sagaMiddleware];
 
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
+if (process.env.NODE_ENV === "development") {
+    middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
@@ -20,4 +20,6 @@ sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
-export default { store, persistStore };
+const modules = { store, persistStore };
+
+export default modules;
